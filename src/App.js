2ffdd050746801657865b58b09fe40c433e564b5
Ugrasen Yadav <ugrasenyadav0024@gmail.com>
2024-08-12 16:34:17 +0530

    final day of 2nd react porject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Component/Navbar.mjs'
+import Texta from './Component/Texta.js'
+//import About from './Component/About.js'
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [mode, setMode]= useState('light');
+
+  const tourch=()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+    }
+    else {setMode('light');
+        document.body.style.backgroundColor = 'white';
+        }
+  };
+  
+  return (<>
+   <Navbar title="UGGU" aboutText="About Us"  mode={mode} tourch={tourch} />
+   <div className="container mx-5">
+    <Texta headin="Case convertor"/>
     </div>
+    
+    </>
   );
 }
 
